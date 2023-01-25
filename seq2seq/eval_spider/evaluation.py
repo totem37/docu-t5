18208a274cd73779969a3b348049b646a063736a
Torrin <2378609m@student.gla.ac.uk>
2023-01-25 18:23:26 +0000

    Added official Spider test suite metric which doesn't work for Spider-DK, and doesn't match what is reported at the end of a run

--- seq2seq/eval_spider/evaluation.py
+++ seq2seq/eval_spider/evaluation.py
@@ -25,6 +25,7 @@ import sqlite3
 import argparse
 
 from process_sql import get_schema, Schema, get_sql
+from exec_eval import eval_exec_match
 
 # Flag to disable value evaluation
 LEVELS = ["easy", "medium", "hard", "extra", "all", "joint_all"]
@@ -426,36 +427,6 @@ def count_others(sql):
     return count
 
 
-def eval_exec_match(db, p_str, g_str, pred, gold):
-    """
-    return 1 if the values between prediction and gold are matching
-    in the corresponding index. Currently not support multiple col_unit(pairs).
-    """
-    conn = sqlite3.connect(db)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(p_str)
-        p_res = cursor.fetchall()
-    except:
-        return False
-
-    try:
-        cursor.execute(g_str)
-        q_res = cursor.fetchall()
-    except:
-        return False
-
-    def res_map(res, val_units):
-        rmap = {}
-        for idx, val_unit in enumerate(val_units):
-            key = tuple(val_unit[1]) if not val_unit[2] else (val_unit[0], tuple(val_unit[1]), tuple(val_unit[2]))
-            rmap[key] = [r[idx] for r in res]
-        return rmap
-
-    p_val_units = [unit[1] for unit in pred['select'][1]]
-    q_val_units = [unit[1] for unit in gold['select'][1]]
-    return res_map(p_res, p_val_units) == res_map(q_res, q_val_units)
-
 class Evaluator:
     """A simple evaluator"""
 
@@ -675,8 +646,6 @@ class Evaluator:
                 self.db_paths[db_name],
                 predicted,
                 gold,
-                p_sql,
-                g_sql
             )
             if exec_score:
                 if self.etype == 'all':
