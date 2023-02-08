6b098362c16175c612e01506d6452c33c2c6be3d
Torrin <2378609m@student.gla.ac.uk>
2023-02-08 15:52:58 +0000

    Spider-Desc optimal experiment

--- experiment/docut5-spider-desc-08-02-2023/predictions.sql
+++ experiment/docut5-spider-desc-08-02-2023/predictions.sql
@@ -1,20 +1,20 @@
-select t1.airportcode from airports as t1 join flights as t2 on t1.city = t2.airport
-select count(*) from pets as t1 join has_pet as t2 on t1.petid = t2.petid join student as t3 on t2.stuid = t3.stuid where t3.age > 20
-select avg(t2.date_left) from transcript_contents as t1 join student_enrolment as t2
-select distinct t1.name from battle as t1 join battle as t2 on t1.id = t2.b
-select t1.first_name, t1.last_name from players as t1 join players as t2 on t1.player_id = t2.player_id where t2.hand = 'L' order by t2.birth_date
-select name from shop where shop_id not in (select shop_id from hiring)
-select count(*) from airports as t1 join flights as t2 on t1.city = t2.destairport where t1.city = "Aberdeen"
-select t1.countryid, t1.countryname from countries as t1 join car_makers as t2 on t1.countryid = t2.country where t2.maker = "Fat" group by t1.countryid having count(*) > 3 union select t1.countryid, t1.countryname from countries as t1 join car_makers as t2 on t1.countryid = t2.country where t2.maker = "Fiat"
-select name, grade from highschooler
-select t1.fname, t1.age from student as t1 join has_pet as t2 on t1.stuid = t2.stuid join pets as t3 on t3.petid = t2.petid where t3.pettype = 'cat'
-select major, age from student where stuid not in (select stuid from has_pet where pettype = 'cat'
-select count(*) from flights
-select winner_name from matches where winner_age = 2013 intersect select winner_name from matches where winner_age = 2016
-select country_code from players group by country_code order by count(*) desc limit 1
-select count(*) from pets where sex = 'F' and stuid in (select stuid from has_pet as t1 join pets as t2 on t1.petid = t2.petid join student as t3 on t3.stuid = t2.stuid
-select t2.name from course_arrange as t1 join teacher as t2 on t1.teacher_id = t2.teacher_id group by t2.name having count(*) >= 2
-select count(*) from orchestra where major_record_format = 'CD' or major_record_format = 'DVD'
-select t1.country_code, t1.first_name from players as t1 join matches as t2 on t1.player_id = t2.player_id
-select state from owners intersect select state from professionals
-select t2.last_name from transcripts as t1 join
+select stuid from has_pet as t1 join student as t2 on t1.stuid = t2.stuid join pets as t3 on t3.pet
+select title from cartoon order by title
+select district from shop where number_products < 3000 intersect select district from shop where number_products > 10000
+select t1.date_of_treatment, t2.first_name from treatments as t1 join professionals as t2 on t1.professional_id = t2.professional_id
+select t2.language from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode where t1.continent = 'Asie' group by t2.language order by count(*) desc limit 1
+select city, country from airports where airportname = "Alton"
+select t1.professional_id, t1.role_code, t1.email_address from professionals as t1 join treatments as t2 on t1.professional_id = t2.professional_id except select t3.professional_id, t3.role_code, t3.email_address from dogs as t1 join treatments as t2 on t1.dog_id = t2.dog_id join professionals as t3 on t2.professional_id = t3.professional_id where t2.date_of_treatment = 'No'
+select distinct t1.name from singer as t1 join song as t2 on t1.singer_id = t2.singer_id where t2.sales > 300000
+select t1.name, t1.country from singer as t1 join singer_in_concert as t2 on
+select t1.name, count(*) from highschooler as t1 join friend as t2 on t1.id = t2.friend_id group by t2.friend_id
+select name, population, lifeexpectancy from country where continent = 'Asie' order by surfacearea desc limit 1
+select t1.name from highschooler as t1 join friend as t2 on t1.id = t2.student_id group by t2.student_id having count(*) >= 3
+select count(*) from country where continent = 'Asie'
+select count(*) from paragraphs as t1 join documents as t2 on t1.document_id = t2.document_id where t2.document_name = 'Summer Show'
+select t2.mpg, avg
+select count(*) from airlines as t1 join flights as t2 on t1.uid = t2.airline where destairport = "Aberdeen"
+select count(*) from airports as t1 join flights as t2 on t1.city = "Aberdeen" or t1.countryabbrev = "Abilene"
+select t3.name from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode join country as t3 on t2.countrycode = t3.code where t1.headofstate = 'Beirut'
+select template_id from documents group by template_id having count(*) > 1
+select count(*) from airlines where country = "USA"
