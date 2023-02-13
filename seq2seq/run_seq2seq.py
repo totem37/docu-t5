985fde3e8f807ffdece762877e0968ddcf1658c7
Torrin <2378609m@student.gla.ac.uk>
2023-02-13 12:30:22 +0000

    Tidy up run_seq2seq.py

--- seq2seq/run_seq2seq.py
+++ seq2seq/run_seq2seq.py
@@ -33,9 +33,7 @@ from seq2seq.utils.spider import SpiderTrainer
 from seq2seq.utils.cosql import CoSQLTrainer
 
 import torch
-import traceback
 from seq2seq.eval_spider.format_predictions import format_predictions
-from seq2seq.utils.torch_module import DistributedDataParallel, Module
 
 # Necessary to prevent "HTTP Error 403: rate limit exceeded" with PyTorch 1.9.0
 torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
@@ -169,25 +167,12 @@ def main() -> None:
         # Initialize model from the given name or path
         model = model_cls_wrapper(AutoModelForSeq2SeqLM).from_pretrained(
             model_args.model_name_or_path,
-            #state_dict=state_dict,
             from_tf=bool(".ckpt" in model_args.model_name_or_path),
             config=config,
             cache_dir=model_args.cache_dir,
             revision=model_args.model_revision,
             use_auth_token=True if model_args.use_auth_token else None,
         )
-
-#         try:
-#             # Attempt to parallelise the model
-#             rank = torch.distributed.get_rank()
-#             device_id = rank % torch.cuda.device_count()
-#             model = model.to(device_id)
-#             model = DistributedDataParallel(model, device_ids=[device_id])
-#         except Exception as e:
-#             print("The following error was thrown when parallelising the model:")
-#             print(e)
-#             print("Full traceback:")
-#             print(traceback.format_exc())
 
         if isinstance(model, T5ForConditionalGeneration):
             model.resize_token_embeddings(len(tokenizer))
@@ -226,13 +211,6 @@ def main() -> None:
             raise NotImplementedError()
 
         trainer = SpiderTrainer(**trainer_kwargs)
-
-        # if data_args.dataset in ["spider"]:
-        #     trainer = SpiderTrainer(**trainer_kwargs)
-        # elif data_args.dataset in ["cosql", "cosql+spider"]:
-        #     trainer = CoSQLTrainer(**trainer_kwargs)
-        # else:
-        #     raise NotImplementedError()
 
         # Training
         if training_args.do_train:
@@ -288,15 +266,6 @@ def main() -> None:
 
             trainer.log_metrics("eval", metrics)
             trainer.save_metrics("eval", metrics)
-
-            # Print full eval metrics for Spider
-            try:
-                if isinstance(trainer, SpiderTrainer):
-                    format_predictions(f"{training_args.output_dir}/predictions_eval_None.json")
-                    os.system(f"cd seq2seq/eval_spider && python evaluation.py --gold {training_args.output_dir}/../../dataset_files/ori_dataset/{data_args.dataset.replace('_', '-')}/dev_gold.sql --pred {training_args.output_dir}/predictions.sql --etype all --db {training_args.output_dir}/../../dataset_files/ori_dataset/{data_args.dataset.replace('_', '-')}/database --table {training_args.output_dir}/../../dataset_files/ori_dataset/{data_args.dataset.replace('_', '-')}/tables.json > {training_args.output_dir}/eval_breakdown.txt && cat {training_args.output_dir}/eval_breakdown.txt")
-            except Exception as e:
-                print(e)
-                print("The detailed evaluation threw an error, skipping.")
 
         # Testing
         if training_args.do_predict:
